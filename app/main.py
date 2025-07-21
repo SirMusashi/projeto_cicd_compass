c4b2bbdff9398fd190a523734046be649da8bb2b
Bruno Duarte <brunopp2@gmail.com>
2025-07-21 16:52:53 -0300

    editado a mensagem final para conferencia

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/") 
 async def root(): 
-    return {"message": "CI/CD com GitHub Actions e ArgoCD! PARA TESTE FINAL"} 
+    return {"message": "Projeto finalizado com sucesso! deu tudo certo (: "} 
